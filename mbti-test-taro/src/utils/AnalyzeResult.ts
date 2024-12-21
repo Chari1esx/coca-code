794a277e163a2e889bed107b724240129821c78b
charix <ari.charlesx@gmail.com>
2024-12-22 00:49:52 +0800

    fix(mbti-test-taro)分析MBTI类型的方法

--- mbti-test-taro/src/utils/AnalyzeResult.ts
+++ mbti-test-taro/src/utils/AnalyzeResult.ts
@@ -2,31 +2,34 @@
  * 获取 MBTI
  */
 export function analyzeMbtiResult(answerList: string[], questions: any, question_results: any) {
-  const optionCount = {};
+  const optionCount = {}
 
-  for (const question of questions) {
-    for (const answer of answerList) {
-      for (const option of question.options) {
-        if (option.key === answer) {
-          const result = option.result;
-          if (!optionCount[result]) {
-            optionCount[result] = 0;
-          }
-          optionCount[result]++;
+  for (let i = 0; i < questions.length && i < answerList.length; i++) {
+    const question = questions[i]
+    const answer = answerList[i]
+    for (const option of question.options) {
+      if (option.key === answer) {
+        const result = option.result
+        if (!optionCount[result]) {
+          optionCount[result] = 0
         }
+        optionCount[result]++
       }
     }
   }
-  let maxScore = 0;
-  let maxScoreResult = question_results[0];
+
+  let maxScore = -1
+  let maxScoreResult = null
   for (const result of question_results) {
     const score = result.resultProp.reduce((count: any, prop: any) => {
-      return count + (optionCount[prop] || 0);
-    }, 0);
+      return count + (optionCount[prop] || 0)
+    }, 0)
     if (score > maxScore) {
-      maxScore = score;
-      maxScoreResult = result;
+      maxScore = score
+      maxScoreResult = result
+    } else if (score === maxScore && maxScoreResult === null) {
+      maxScoreResult = result
     }
   }
-  return maxScoreResult;
+  return maxScoreResult
 }
